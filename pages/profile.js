41ddeffc5e6495f0e4cdd941ca582f273ff5dd60
Whitley Beers <whitleybeers@gmail.com>
2023-02-28 09:40:36 -0600

    user profile created and tested

--- pages/profile.js
+++ pages/profile.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import UserProfile from '../components/UserProfile';
 
 export default function ProfileView() {
   return (
-    <div>UserProfile</div>
+    <UserProfile />
   );
 }
