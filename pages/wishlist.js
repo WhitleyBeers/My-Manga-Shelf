a0fcb91ebf4fae17baaa568761be417ad226b68a
Whitley Beers <whitleybeers@gmail.com>
2023-03-01 22:08:00 -0600

    button added to wishlist page to route to new series search

--- pages/wishlist.js
+++ pages/wishlist.js
@@ -1,4 +1,6 @@
+import { useRouter } from 'next/router';
 import React, { useEffect, useState } from 'react';
+import { Button, ButtonGroup } from 'react-bootstrap';
 import { getAllSeries } from '../api/seriesData';
 import SeriesWishlistCards from '../components/cards/SeriesWishlistCards';
 import { useAuth } from '../utils/context/authContext';
@@ -6,6 +8,7 @@ import { useAuth } from '../utils/context/authContext';
 export default function WishlistView() {
   const { user } = useAuth();
   const [series, setSeries] = useState([]);
+  const router = useRouter();
 
   const getAllTheSeries = () => {
     getAllSeries(user.uid).then(setSeries);
@@ -18,6 +21,11 @@ export default function WishlistView() {
   return (
     <div className="text-center">
       <h1 className="my-4">Your Wishlist</h1>
+      <ButtonGroup>
+        <Button className="btn-blue" onClick={() => router.push('/new')}>
+          Add New Series
+        </Button>
+      </ButtonGroup>
       <div className="my-2 d-flex justify-content-center flex-wrap">
         {series.map((singleSeries) => (
           <SeriesWishlistCards key={singleSeries.firebaseKey} seriesObj={singleSeries} onUpdate={getAllTheSeries} />
